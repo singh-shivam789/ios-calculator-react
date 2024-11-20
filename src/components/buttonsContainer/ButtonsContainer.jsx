5ab06d4f678354b83ca1f535d5304f9b20a6c961
Shivam Singh <shivamsinghgbpit@gmail.com>
2024-11-20 21:38:08 +0530

    Some Minor fixes

--- src/components/buttonsContainer/ButtonsContainer.jsx
+++ src/components/buttonsContainer/ButtonsContainer.jsx
@@ -3,34 +3,34 @@ import Button from './button/button'
 export default function ButtonsContainer() {
     return (
         <div className='buttons-container'>
-            <Button fn="clear" bgColor="gray" fontColor="black" name="AC" />
-            <Button fn="negate" bgColor="gray" fontColor="black" name="+/-" />
-            <Button fn="percentage" bgColor="gray" fontColor="black" name="%" />
+            <Button fn="clear" bgColor="#808080" fontColor="black" value="AC" />
+            <Button fn="negate" bgColor="#808080" fontColor="black" value="+/-" />
+            <Button fn="percentage" bgColor="#808080" fontColor="black" value="%" />
 
-            <Button fn="operator" bgColor="orange" fontColor="whitesmoke" name="/" />
+            <Button fn="operator" bgColor="#FFA500" fontColor="whitesmoke" value="/" />
 
-            <Button fn="num" bgColor="rgb(59, 59, 59)" fontColor="whitesmoke" name="7" />
-            <Button fn="num" bgColor="rgb(59, 59, 59)" fontColor="whitesmoke" name="8" />
-            <Button fn="num" bgColor="rgb(59, 59, 59)" fontColor="whitesmoke" name="9" />
+            <Button fn="num" bgColor="#3B3B3B" fontColor="whitesmoke" value="7" />
+            <Button fn="num" bgColor="#3B3B3B" fontColor="whitesmoke" value="8" />
+            <Button fn="num" bgColor="#3B3B3B" fontColor="whitesmoke" value="9" />
 
-            <Button fn="operator" bgColor="orange" fontColor="whitesmoke" name="X" />
+            <Button fn="operator" bgColor="#FFA500" fontColor="whitesmoke" value="X" />
 
-            <Button fn="num" bgColor="rgb(59, 59, 59)" fontColor="whitesmoke" name="4" />
-            <Button fn="num" bgColor="rgb(59, 59, 59)" fontColor="whitesmoke" name="5" />
-            <Button fn="num" bgColor="rgb(59, 59, 59)" fontColor="whitesmoke" name="6" />
+            <Button fn="num" bgColor="#3B3B3B" fontColor="whitesmoke" value="4" />
+            <Button fn="num" bgColor="#3B3B3B" fontColor="whitesmoke" value="5" />
+            <Button fn="num" bgColor="#3B3B3B" fontColor="whitesmoke" value="6" />
 
-            <Button fn="operator" bgColor="orange" fontColor="whitesmoke" name="-" />
+            <Button fn="operator" bgColor="#FFA500" fontColor="whitesmoke" value="-" />
 
-            <Button fn="num" bgColor="rgb(59, 59, 59)" fontColor="whitesmoke" name="1" />
-            <Button fn="num" bgColor="rgb(59, 59, 59)" fontColor="whitesmoke" name="2" />
-            <Button fn="num" bgColor="rgb(59, 59, 59)" fontColor="whitesmoke" name="3" />
+            <Button fn="num" bgColor="#3B3B3B" fontColor="whitesmoke" value="1" />
+            <Button fn="num" bgColor="#3B3B3B" fontColor="whitesmoke" value="2" />
+            <Button fn="num" bgColor="#3B3B3B" fontColor="whitesmoke" value="3" />
 
-            <Button fn="operator" bgColor="orange" fontColor="whitesmoke" name="+" />
+            <Button fn="operator" bgColor="#FFA500" fontColor="whitesmoke" value="+" />
 
-            <Button fn="num" isZero={true} bgColor="rgb(59, 59, 59)" fontColor="whitesmoke" name="0" />
-            <Button fn="decimal" bgColor="rgb(59, 59, 59)" fontColor="whitesmoke" name="." />
+            <Button fn="num" isZero={true} bgColor="#3B3B3B" fontColor="whitesmoke" value="0" />
+            <Button fn="decimal" bgColor="#3B3B3B" fontColor="whitesmoke" value="." />
 
-            <Button fn="result" bgColor="orange" fontColor="whitesmoke" name="=" />
+            <Button fn="result" bgColor="#FFA500" fontColor="whitesmoke" value="=" />
         </div>
     )
 }
